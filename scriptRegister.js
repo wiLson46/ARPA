a6f93b31bf1cac1a76dd011acfbd21cf354b69c4
wiLson <a@b.com>
2022-10-20 21:58:09 -0300

    updates

--- scriptRegister.js
+++ scriptRegister.js
@@ -1,23 +1,27 @@
 function register() {
-    fetch('http://arpaweb.ddns.net:8080/login', {
+    const [day, month, year] = document.getElementById("inputDate").value.split('/')
+    const date = new Date(+year, month - 1, +day);
+    fetch('http://arpaweb.ddns.net:8080/nuevousuario', {
         method: 'POST',
         body: JSON.stringify({
             "nombre": document.getElementById("inputNombre").value + " " + document.getElementById("inputApellido").value,
-            //"apellido": document.getElementById("inputApellido").value,
             "dni": document.getElementById("inputDNI").value,
             "telefono": document.getElementById("inputPhone").value,
-            "fechaNacimiento": Date.parse(document.getElementById("inputDate").value),
+            "fechaNacimiento": date,
             //"domicilio": document.getElementById("").value,
-            "contraseÃ±a": document.getElementById("inputPass").value,
+            "contraseña": document.getElementById("inputPass").value,
             //"usuario": document.getElementById("").value,
             "mail": document.getElementById("inputEmail").value,
         }),
         headers: {
             "Content-type": "application/json",
         }
-    }).then(response => response.text())
-    .then(text => document.getElementById("resp").innerText = text)
-    const [day, month, year] = document.getElementById("inputDate").value.split('/')
-    const date = new Date(+year, month - 1, +day);
-    console.log(date)
+    }).then(response => response)
+            .then(resp =>{
+                if (resp.status === 200){
+                    window.location.href = 'index.html'
+        } else {
+            //aca se usaria resp.json().mensaje para capturar la respeusta de error y mandarla al toast o a otra parte
+        }
+    });
 }
